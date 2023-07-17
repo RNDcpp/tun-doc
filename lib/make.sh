cbd894d1ec0dae8c9880a346eac20473132648f5
RNDcpp <yonbanki@gmail.com>
2023-07-17 23:03:11 +0900

    feat: 変更を反映してpull request messageを出力するようにスクリプトを変更

--- lib/make.sh
+++ lib/make.sh
@@ -1,4 +1,4 @@
 row=$( lib/token/read-files.sh | lib/token/get-row-number.sh )
-lib/scripts/get-update-file.sh $row
-lib/scripts/get-update-title.sh $row
+title=$( lib/scripts/get-update-title.sh $row )
 lib/scripts/update.sh $row
+cat .github/TEMPLATE.md | sed -r "s|update-title|${title}|g"
